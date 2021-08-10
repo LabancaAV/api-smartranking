6f0f1ea08eedc258bd0e8c683e2d3d1d640d1453
Andre Labanca <andreventura@brasilcard.net>
2021-08-10 11:45:07 -0300

    feat: adição de validações ao cadastrar novo jogador.

--- src/jogadores/dtos/criar-jogador.dto.ts
+++ src/jogadores/dtos/criar-jogador.dto.ts
@@ -1,5 +1,10 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class CriarJogadorDto {
+  @IsNotEmpty()
   readonly telefoneCelular: string;
+  @IsEmail()
   readonly email: string;
+  @IsNotEmpty()
   readonly nome: string;
 }
--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Delete, Get, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 import { Jogador } from './interfaces/jogador.interface';
 import { JogadoresService } from './jogadores.service';
@@ -8,6 +17,7 @@ export class JogadoresController {
   constructor(private readonly jogadoresService: JogadoresService) {}
 
   @Post()
+  @UsePipes(ValidationPipe)
   async criarAtualizarJogador(@Body() criarJogadorDto: CriarJogadorDto) {
     await this.jogadoresService.criarAtualizarJogador(criarJogadorDto);
   }
