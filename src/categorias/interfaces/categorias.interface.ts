57506d33bc324978f10be17caf480e558c7c0ab3
unknown <andrelabanca2@gmail.com>
2021-08-12 19:49:07 -0300

    feat: criação da interface categorias.

---
+++ src/categorias/interfaces/categorias.interface.ts
@@ -0,0 +1,18 @@
+import { Jogador } from "src/jogadores/interfaces/jogador.interface";
+
+export interface Categoria{
+
+    readonly categoria: string;
+    descrição: string;
+    eventos: Array<Evento>;
+    jogadores: Array<Jogador>
+
+}
+
+export interface Evento{
+
+    nome: string;
+    operação: string;
+    valor: number;
+
+}
