1283d82d62fa19dd99c82d4ea2b93d962204ff40
Andre Labanca <andreventura@brasilcard.net>
2021-08-18 08:37:49 -0300

    fix: exclusão de importações desnecessárias.

--- src/jogadores/jogadores.service.ts
+++ src/jogadores/jogadores.service.ts
@@ -1,8 +1,8 @@
 import {
-  BadRequestException,
-  Injectable,
-  Logger,
-  NotFoundException,
+    BadRequestException,
+    Injectable,
+    Logger,
+    NotFoundException,
 } from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 import { Jogador } from './interfaces/jogador.interface';
@@ -11,81 +11,80 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { JogadorEntity } from './entities/jogador.entity';
 import { AtualizarJogadorDto } from './dtos/atualizar-jogador.dto';
-import { Categoria } from 'src/categorias/interfaces/categorias.interface';
-import { CategoriaEntity } from 'src/categorias/entities/categorias.entity';
 
 @Injectable()
 export class JogadoresService {
-  constructor(
-    @InjectRepository(JogadorEntity)
-    private readonly jogadorRepository: Repository<Jogador>,
-  ) {}
+    constructor(
+        @InjectRepository(JogadorEntity)
+        private readonly jogadorRepository: Repository<Jogador>,
+    ) {}
 
-  private readonly logger = new Logger(JogadoresService.name);
+    private readonly logger = new Logger(JogadoresService.name);
 
-  async criarJogador(criarJogadorDto: CriarJogadorDto): Promise<Jogador> {
+    async criarJogador(criarJogadorDto: CriarJogadorDto): Promise<Jogador> {
+        const { email } = criarJogadorDto;
+        const jogadorEncontrado = await this.jogadorRepository.findOne({
+            where: {
+                email: email,
+            },
+        });
 
-    const { email } = criarJogadorDto;
-    const jogadorEncontrado = await this.jogadorRepository.findOne({
-      where: {
-        email: email,
-      },
-    });
+        if (jogadorEncontrado) {
+            throw new BadRequestException(
+                `Jogador com e-mail ${email} já cadastrado`,
+            );
+        }
 
-    if (jogadorEncontrado) {
-      throw new BadRequestException(
-        `Jogador com e-mail ${email} já cadastrado`,
-      );
-    }
-   
-    const jogador: Jogador = {
-      _id: uuid(),
-      ...criarJogadorDto,
-      ranking: 'A',
-      posicaoRanking: 1,
-      urlFotoJogador: 'www.google.com.br/foto123.jpg'
-    };
+        const jogador: Jogador = {
+            _id: uuid(),
+            ...criarJogadorDto,
+            ranking: 'A',
+            posicaoRanking: 1,
+            urlFotoJogador: 'www.google.com.br/foto123.jpg',
+        };
 
-    this.logger.log(`criaJogadorDto: ${JSON.stringify(jogador)}`);
+        this.logger.log(`criaJogadorDto: ${JSON.stringify(jogador)}`);
 
-    return this.jogadorRepository.save(jogador);
-  }
-
-  async atualizarJogador(
-    _id: string,
-    atualizarJogadorDto: AtualizarJogadorDto,
-  ): Promise<void> {
-    const jogadorEncontrado = await this.jogadorRepository.findOne(_id);
-
-    if (!jogadorEncontrado) {
-      throw new NotFoundException(`Jogador com ${_id} não encontrado`);
+        return this.jogadorRepository.save(jogador);
     }
 
-    this.jogadorRepository.update(_id, atualizarJogadorDto);
-  }
+    async atualizarJogador(
+        _id: string,
+        atualizarJogadorDto: AtualizarJogadorDto,
+    ): Promise<void> {
+        const jogadorEncontrado = await this.jogadorRepository.findOne(_id);
 
-  async consultarTodosJogadores(): Promise<Jogador[]> {
-    return await this.jogadorRepository.find({
-      relations: ['categoria']
-    });
-  }
+        if (!jogadorEncontrado) {
+            throw new NotFoundException(`Jogador com ${_id} não encontrado`);
+        }
 
-  async consultarJogadorPeloId(_id: string): Promise<Jogador> {
-    const jogadorEncontrado = await this.jogadorRepository.findOne(_id);
-    if (!jogadorEncontrado) {
-      throw new NotFoundException(`Jogador com e-mail ${_id} não encontrado`);
-    } else {
-      return jogadorEncontrado;
+        this.jogadorRepository.update(_id, atualizarJogadorDto);
     }
-  }
 
-  async deletarJogador(_id: string): Promise<void> {
-    const jogadorEncontrado = await this.jogadorRepository.findOne(_id);
-    if (!jogadorEncontrado) {
-      throw new NotFoundException(`Jogador com e-mail ${_id} não encontrado`);
+    async consultarTodosJogadores(): Promise<Jogador[]> {
+        return await this.jogadorRepository.find({
+            relations: ['categoria'],
+        });
     }
-    this.jogadorRepository.delete(jogadorEncontrado);
-  }
 
+    async consultarJogadorPeloId(_id: string): Promise<Jogador> {
+        const jogadorEncontrado = await this.jogadorRepository.findOne(_id);
+        if (!jogadorEncontrado) {
+            throw new NotFoundException(
+                `Jogador com e-mail ${_id} não encontrado`,
+            );
+        } else {
+            return jogadorEncontrado;
+        }
+    }
 
+    async deletarJogador(_id: string): Promise<void> {
+        const jogadorEncontrado = await this.jogadorRepository.findOne(_id);
+        if (!jogadorEncontrado) {
+            throw new NotFoundException(
+                `Jogador com e-mail ${_id} não encontrado`,
+            );
+        }
+        this.jogadorRepository.delete(jogadorEncontrado);
+    }
 }
